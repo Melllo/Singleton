136834af1fa9e87eea2158302f69d2cbc92e8862
Irene Herbei <trisha19991410@gmail.com>
2021-01-19 13:52:27 +0200

    Singleton

---
+++ FileStreamSingleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class FileStreamSingleton
+    {
+        private static readonly Lazy<FileStreamSingleton> instance = new Lazy<FileStreamSingleton>(() => new FileStreamSingleton());
+
+        public string FilePath { get; }
+        public string Text { get; set; }
+
+        private FileStreamSingleton() {
+            FilePath = "tasks.txt";
+        }
+
+        public static FileStreamSingleton getInstance() {
+            return instance.Value;
+        }
+
+        public void WriteTask(string text) {
+            Text += text;
+        }
+
+        public void Save() {
+            using (var writer = new StreamWriter(new FileStream(FilePath, FileMode.Append, FileAccess.Write))) {
+                writer.WriteLine(Text);
+                Text = "";
+            }
+        }
+
+        public void ReadText() {
+            if (File.Exists(FilePath))
+            {
+                string alltext = "";
+                using (var reader = new StreamReader(FilePath)) {
+                    alltext = reader.ReadToEnd();
+                    Console.WriteLine(alltext);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text1 = FileStreamSingleton.getInstance();
+            string s = "";
+            for (; ;) {
+                
+                Console.WriteLine("Write new task in list(write / for show all list)");
+                s = Console.ReadLine();
+                if (s.Equals("/"))
+                {
+                    text1.ReadText();
+                }
+                else {
+                    text1.WriteTask("+ " + s);
+                    text1.Save();
+                }
+            }
+            
+            
+           // text1.WriteTask("dsfdfsdf");
+            //text1.Save();
+            text1.ReadText();
+        }
+    }
+}
